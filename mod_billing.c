a2b6431a53a09f585e07e1ce98345ffc3cd7b510
Emanuell Faustino <emanuellfaustino@gmail.com>
2010-09-24 00:59:02 +0000

    billing for subpaths, refactor and improvements

--- mod_billing.c
+++ mod_billing.c
@@ -1,57 +1,106 @@
+//many includes...
+/*
+TODO
+
+- Segmentation fault when more than one request is done.
+*/
+
+
+
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 #include "apr_want.h"
-#include "apr_general.h"    /* for APR_OFFSETOF                */
+#include "apr_general.h"
 #include "apr_network_io.h"
 
 #include "httpd.h"
 #include "http_config.h"
 #include "http_log.h"
-#include "http_protocol.h"  /* for ap_hook_post_read_request */
+#include "http_protocol.h"
+#include "apr_uri.h"
 
+module AP_MODULE_DECLARE_DATA billing_module;
 
-static void register_pid(apr_pool_t *p, server_rec *s)
-{
-	pid_t pid;
-	
-	pid = getpid();
-	
-	ap_log_error(APLOG_MARK, APLOG_ALERT, 0, s, "CLOUD-UFPE: Process created - pid: %d", (int)pid);
-}
+typedef struct billing_t {
+	const char* url;
+} billing_t;
+
 
 static int init_time(request_rec *r)
 {
-	apr_table_set(r->notes, "clockinit", clock());
-	ap_log_error(APLOG_MARK, APLOG_ALERT, 0, NULL, "CLOUD-UFPE: Init CPU time process %6.9f", (double) clock());
-
+	billing_t *bil = ap_get_module_config(r->per_dir_config, &billing_module);	
+	
+	/*
+		If the first path level match specified uri. 
+		Ex: billing for /app1; uri=/app1/images/picture.png
+	*/
+	char *path_found = strstr(r->uri, bil->url);
+	if(path_found && (path_found - r->uri) == 0) {
+		apr_table_set(r->notes, "clockinit", (clock_t) clock());
+		apr_table_set(r->notes, "billing", "y");		
+	} else {
+		apr_table_set(r->notes, "billing", "n");
+	}
+	
 	return OK;
 }
 
 static int end_time(request_rec *r)
 {
-	clock_t init_time = (clock_t) apr_table_get(r->notes, "clockinit");
-	clock_t end_time = clock();	
+	if(strcmp(apr_table_get(r->notes, "billing"), "y") == 0) {
 
-	double elapsed_time = end_time - init_time;
-	ap_log_error(APLOG_MARK, APLOG_ALERT, 0, NULL, "CLOUD-UFPE: End request process - Request CPU time: %6.9f", elapsed_time);
-	ap_log_error(APLOG_MARK, APLOG_ALERT, 0, NULL, "CLOUD-UFPE: End request process - Request CPU time in secs: %6.9f", ((end_time - init_time) / (double) CLOCKS_PER_SEC));
+		clock_t init_time = (clock_t) apr_table_get(r->notes, "clockinit");
+		
+		billing_t *bil = ap_get_module_config(r->per_dir_config, &billing_module);
+		
+		clock_t end_time = clock();
+		double elapsed_time = ((double) (end_time - init_time));
+		
+		ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, NULL, "[Billing %s][URI: %s]: Request CPU time: %f", bil->url, r->uri, elapsed_time);
+		//ap_log_error(APLOG_MARK, APLOG_ALERT, 0, NULL, "CLOUD-UFPE: End request process - Request CPU time in secs: %6.9f", ((end_time - init_time) / (double) CLOCKS_PER_SEC));
+	}
 
 	return OK;
 }
 
+static const char* billing_set(cmd_parms* cmd, void* cfg, const char* val) {
+	billing_t *bil = ((billing_t *)cfg);
+	bil->url = val;
+
+	int len = strlen(bil->url);
+	if(bil->url[len-1] != '/') {
+		strncat(bil->url, "/", 1);
+	}
+
+	return NULL;
+}
+
+static void* billing_create_cfg(apr_pool_t *pool, char *x) {
+	billing_t *st = apr_pcalloc(pool, sizeof(billing_t));
+	st->url = "/";	
+
+	return st;
+}
+
+static const command_rec billing_cmds[] = {
+	AP_INIT_TAKE1("Billing", billing_set, NULL, OR_ALL, "Billing for a specific URL"),
+	{NULL}
+};
+
 static void register_hooks(apr_pool_t *p)
 {
-	ap_hook_child_init(register_pid, NULL, NULL, APR_HOOK_MIDDLE);
+	//ap_hook_child_init(register_pid, NULL, NULL, APR_HOOK_MIDDLE);
 	ap_hook_post_read_request(init_time, NULL, NULL, APR_HOOK_MIDDLE);
 	ap_hook_log_transaction(end_time, NULL, NULL, APR_HOOK_MIDDLE);
 }
 
-module AP_MODULE_DECLARE_DATA billing_module = {
+AP_MODULE_DECLARE_DATA module billing_module = {
     STANDARD20_MODULE_STUFF,
-    NULL,                       /* dir config creater */
+    billing_create_cfg,         /* dir config creater */
     NULL,                       /* dir merger --- default is to override */
     NULL,                       /* server config */
     NULL,                       /* merge server configs */
-    NULL,                       /* command apr_table_t */
+    billing_cmds,               /* command apr_table_t */
     register_hooks              /* register hooks */
 };
