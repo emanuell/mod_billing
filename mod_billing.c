26cb6f57e8d0f886ca24b4220fed2d87e867053a
Emanuell Faustino <emanuellfaustino@gmail.com>
2010-10-05 10:27:49 -0300

    fixed segmentation fault

--- mod_billing.c
+++ mod_billing.c
@@ -1,8 +1,7 @@
-//many includes...
 /*
 TODO
-
-- Segmentation fault when more than one request is done.
+ - many includes...
+ - many warnings
 */
 
 
@@ -10,9 +9,11 @@ TODO
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include "apr_strings.h" 
 #include "apr_want.h"
 #include "apr_general.h"
 #include "apr_network_io.h"
+#include "apr_pools.h"
 
 #include "httpd.h"
 #include "http_config.h"
@@ -23,7 +24,7 @@ TODO
 module AP_MODULE_DECLARE_DATA billing_module;
 
 typedef struct billing_t {
-	const char* url;
+	char* url;
 } billing_t;
 
 
@@ -32,15 +33,18 @@ static int init_time(request_rec *r)
 	billing_t *bil = ap_get_module_config(r->per_dir_config, &billing_module);	
 	
 	/*
-		If the first path level match specified uri. 
-		Ex: billing for /app1; uri=/app1/images/picture.png
+	  If the first path level match specified uri. 
+	  Ex: billing for /app1; uri=/app1/images/picture.png
 	*/
 	char *path_found = strstr(r->uri, bil->url);
 	if(path_found && (path_found - r->uri) == 0) {
-		apr_table_set(r->notes, "clockinit", (clock_t) clock());
-		apr_table_set(r->notes, "billing", "y");		
+		
+		clock_t init_time = clock();
+		apr_pool_userdata_set((clock_t*)init_time, "clockinit", NULL, r->pool);
+		apr_pool_userdata_set("y", "billing", NULL, r->pool);
+	
 	} else {
-		apr_table_set(r->notes, "billing", "n");
+		apr_pool_userdata_set("n", "billing", NULL, r->pool);
 	}
 	
 	return OK;
@@ -48,17 +52,21 @@ static int init_time(request_rec *r)
 
 static int end_time(request_rec *r)
 {
-	if(strcmp(apr_table_get(r->notes, "billing"), "y") == 0) {
+	void* tax;
+	apr_pool_userdata_get(&tax, "billing", r->pool);	
 
-		clock_t init_time = (clock_t) apr_table_get(r->notes, "clockinit");
+	if(strcmp(tax, "y") == 0) {
+
+		clock_t init_time;
+		apr_pool_userdata_get(&init_time, "clockinit", r->pool);	
 		
 		billing_t *bil = ap_get_module_config(r->per_dir_config, &billing_module);
 		
 		clock_t end_time = clock();
 		double elapsed_time = ((double) (end_time - init_time));
 		
-		ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, NULL, "[Billing %s][URI: %s]: Request CPU time: %f", bil->url, r->uri, elapsed_time);
-		//ap_log_error(APLOG_MARK, APLOG_ALERT, 0, NULL, "CLOUD-UFPE: End request process - Request CPU time in secs: %6.9f", ((end_time - init_time) / (double) CLOCKS_PER_SEC));
+		ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, NULL, "[Billing %s][URI: %s]: Request CPU time: %6.3f", bil->url, r->uri, (elapsed_time / (double) CLOCKS_PER_SEC));
+	
 	}
 
 	return OK;
@@ -66,11 +74,22 @@ static int end_time(request_rec *r)
 
 static const char* billing_set(cmd_parms* cmd, void* cfg, const char* val) {
 	billing_t *bil = ((billing_t *)cfg);
-	bil->url = val;
+	
+	if (val != NULL) {
+	
+		//char* tmp = apr_palloc(cmd->pool, sizeof(char));
+		char* tmp = malloc((strlen(val) + 1) * sizeof(char));
+		apr_cpystrn(tmp, val, strlen(val) + 1); // +1 because null terminator.
+		
+		bil->url = tmp;
+		
+		int len = strlen(bil->url);
+		if(bil->url[len-1] != '/') {
+			strncat(bil->url, "/", 1);
+		}
 
-	int len = strlen(bil->url);
-	if(bil->url[len-1] != '/') {
-		strncat(bil->url, "/", 1);
+	} else {
+		bil->url = "/";
 	}
 
 	return NULL;
