fe7ecabcfc1d35da63dca426862f3e86e6ab3bbb
Emanuell Faustino <emanuellfaustino@gmail.com>
2010-10-20 17:09:56 -0300

    monitor and tests

---
+++ test/batch/billingserver.py
@@ -0,0 +1,48 @@
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+	
+	def do_GET(self):
+		print 'GET' + self.path
+		
+		self.do_POST()
+		return
+		
+	
+	def do_POST(self):
+		self.send_response(200)
+		
+		self.send_header('Content-type', 'text/html')	
+		self.end_headers()
+		
+		params = self.get_params(self.rfile, int(self.headers['Content-Length']))
+		
+		app_name = params['app']
+		billing_value = params['value']
+		
+		self.wfile.write('guid:app:%s:value:%s:aisodasdlldkf' % (app_name, billing_value))
+		
+		return
+	
+	def get_params(self, inputstream, length):
+		param_as_stream = inputstream.read(length)
+		params_as_array = param_as_stream.split('&')
+		
+		params = {}
+		for p in params_as_array:
+			key = p.split('=')[0]
+			value = p.split('=')[1]
+			
+			params[key] = value
+		
+		return params
+	
+
+
+def main():
+	server = HTTPServer(('localhost', 7777), Handler)
+	print 'listening...'
+	server.serve_forever()
+
+if __name__ == '__main__':
+	main()
