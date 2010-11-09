e9ac45660877f56b520e780415bca0212d663387
Emanuell Faustino <emanuellfaustino@gmail.com>
2010-11-09 13:03:37 -0300

    fix transfer data between hooks problem

--- mod_billing.c
+++ mod_billing.c
@@ -1,31 +1,20 @@
-/*
-TODO
- - many includes...
- - many warnings
-*/
-
-
-
-#include <unistd.h>
-#include <time.h>
 #include <string.h>
-#include "apr_strings.h" 
-#include "apr_want.h"
-#include "apr_general.h"
-#include "apr_network_io.h"
-#include "apr_pools.h"
-
 #include "httpd.h"
 #include "http_config.h"
 #include "http_log.h"
 #include "http_protocol.h"
-#include "apr_uri.h"
+
 
 module AP_MODULE_DECLARE_DATA billing_module;
 
 typedef struct billing_t {
-	char* url;
+	char *url;
 } billing_t;
+
+typedef struct rcpu_t {
+	clock_t init;
+	clock_t end;
+} rcpu_t;
 
 
 static int init_time(request_rec *r)
@@ -38,11 +27,16 @@ static int init_time(request_rec *r)
 	*/
 	char *path_found = strstr(r->uri, bil->url);
 	if(path_found && (path_found - r->uri) == 0) {
-		
 		clock_t init_time = clock();
-		apr_pool_userdata_set((clock_t*)init_time, "clockinit", NULL, r->pool);
+		
+		rcpu_t *consumption = apr_palloc(r->pool, sizeof(rcpu_t));
+		consumption->init = init_time;
+
+		char key[35];
+		sprintf(key, "%f", (float)r->request_time);
+
+		apr_pool_userdata_set((const void *)consumption, key, NULL, r->pool);
 		apr_pool_userdata_set("y", "billing", NULL, r->pool);
-	
 	} else {
 		apr_pool_userdata_set("n", "billing", NULL, r->pool);
 	}
@@ -52,18 +46,24 @@ static int init_time(request_rec *r)
 
 static int end_time(request_rec *r)
 {
-	void* tax;
-	apr_pool_userdata_get(&tax, "billing", r->pool);	
+	void *tax;
+	apr_pool_userdata_get(&tax, "billing", r->pool);
 
 	if(strcmp(tax, "y") == 0) {
 
-		clock_t init_time;
-		apr_pool_userdata_get(&init_time, "clockinit", r->pool);	
+		billing_t *bil;
+		rcpu_t *consumption;
+		void *data;
+		char key[35];
+
+		sprintf(key, "%f", (float)r->request_time);
+		apr_pool_userdata_get(&data, key, r->pool);
+
+		consumption = (rcpu_t *)data;
+		bil = ap_get_module_config(r->per_dir_config, &billing_module);
 		
-		billing_t *bil = ap_get_module_config(r->per_dir_config, &billing_module);
-		
-		clock_t end_time = clock();
-		double elapsed_time = ((double) (end_time - init_time));
+		consumption->end = clock();
+		double elapsed_time = ((double) (consumption->end - consumption->init));
 		
 		ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, NULL, "[Billing %s][URI: %s]: Request CPU time: %6.3f", bil->url, r->uri, (elapsed_time / (double) CLOCKS_PER_SEC));
 	
